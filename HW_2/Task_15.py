de0d8d4593a876f37f43365a21d177abe341736f
Oleg-Storozhev <olegStoroshev@gmail.com>
2021-08-20 23:05:41 +0300

    I have done the second homework

--- HW_2/Task_15.py
+++ HW_2/Task_15.py
@@ -9,3 +9,8 @@ def matrix_mul(a, b):
             row.append(x)
         answ.append(row)
     return answ
+
+
+assert matrix_mul([[1, 2], [3, 2]], [[3, 2], [1, 1]]) == [[5, 4], [11, 8]]
+assert matrix_mul([[9, 7], [0, 1]], [[1, 1], [4, 12]]) == [[37, 93], [4, 12]]
+assert matrix_mul([[1, 2, 3], [3, 2, 1], [2, 1, 3]], [[4, 5, 6], [6, 5, 4], [4, 6, 5]]) == [[28, 33, 29], [28, 31, 31], [26, 33, 31]]
